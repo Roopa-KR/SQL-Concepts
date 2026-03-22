aad5ec3c11e50ce9ca8e67341fd7b36d1b2c8494
Roopa-kr <nisharoopa3@gmail.com>
2026-03-22 22:22:23 +0530

    Triggers

--- Triggers.sql
+++ Triggers.sql
@@ -1,1 +1,12 @@
 create DATABASE IF NOT EXISTS `Triggers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+use Triggers;
+ create table employees( 
+ emp_id bigint ,
+ name varchar(100),
+ salary float
+ );
+
+ create table logged_data(
+ log_id bigint primary key auto_increment,
+ created_at CURRENT_TIMESTAMP default CURRENT_TIME
